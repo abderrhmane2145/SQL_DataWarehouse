d696a3201ae37ff8fd585baab98ff1a621f9c5a4
Abderrhmane Ch <abderrhmanechanderli22@gmail.com>
2025-08-12 22:08:25 +0100

    Create ddl_bronez.sql

---
+++ scripts/bronez/ddl_bronez.sql
@@ -0,0 +1,74 @@
+/*
+==========================================================================================
+DDL Scripts : Create Bronez Tables
+==========================================================================================
+script Purpose : 
+  This script will create tables in Schema 'bronez',Dropping existing tables 
+  if they are already exist.
+  Run this script will re-define the DDL structure of 'bronez' tables.
+==========================================================================================
+*/
+
+-- For CRM
+  IF OBJECT_ID('bronez.crm_cust_info', 'U') IS NOT NULL
+     DROP TABLE bronez.crm_cust_info;
+  CREATE TABLE bronez.crm_cust_info(
+  	cst_id  INT,
+  	cst_key         NVARCHAR(50),
+  	cst_firstname   NVARCHAR(50),
+  	cst_lastname    NVARCHAR(50),
+  	cst_marital_status NVARCHAR(50),
+  	cst_gndr NVARCHAR(50),
+  	cst_create_date DATE
+  );
+  
+  IF OBJECT_ID('bronez.crm_prd_info', 'U') IS NOT NULL
+     DROP TABLE bronez.crm_prd_info;
+  CREATE TABLE bronez.crm_prd_info (
+  	prd_id       INT,
+  	prd_key      NVARCHAR(50),
+  	prd_nm       NVARCHAR(50),
+  	prd_cost     INT,
+  	prd_line     NVARCHAR(50),
+  	prd_start_dt DATETIME,
+  	prd_end_dt   DATETIME
+  );
+  
+  IF OBJECT_ID('bronez.crm_sales_details', 'U') IS NOT NULL
+     DROP TABLE bronez.crm_sales_details;
+  CREATE TABLE bronez.crm_sales_details (
+  	sls_ord_num  NVARCHAR(50),
+  	sls_prd_key  NVARCHAR(50),
+  	sls_cust_id  INT,
+  	sls_order_dt INT,
+  	sls_ship_dt  INT,
+  	sls_due_dt   INT,
+  	sls_sales    INT,
+  	sls_quantity INT,
+  	sls_price    INT
+  );
+
+-- For ERP
+  IF OBJECT_ID('bronez.epr_cust_az12', 'U') IS NOT NULL
+     DROP TABLE bronez.epr_cust_az12;
+  CREATE TABLE bronez.epr_cust_az12 (
+  	CID   NVARCHAR(50),
+  	BDATE DATE,
+  	GEN   NVARCHAR(50)
+  );
+  
+  IF OBJECT_ID('bronez.epr_loc_a101', 'U') IS NOT NULL
+     DROP TABLE bronez.epr_loc_a101;
+  CREATE TABLE bronez.epr_loc_a101 (
+  	CID   NVARCHAR(50),
+  	CNTRY NVARCHAR(50)
+  );
+  
+  IF OBJECT_ID('bronez.epr_px_cat_g1v2', 'U') IS NOT NULL
+     DROP TABLE bronez.epr_px_cat_g1v2;
+  CREATE TABLE bronez.epr_px_cat_g1v2 (
+  	ID          NVARCHAR(50),
+  	CAT         NVARCHAR(50),
+  	SUBCAT      NVARCHAR(50),
+  	MAINTENANCE NVARCHAR(50)
+  );
